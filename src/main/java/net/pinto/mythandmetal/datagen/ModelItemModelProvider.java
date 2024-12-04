a0e0bf217a2511fc408116acbd4465d5a7375455
crowmoed <notcristianopinto@gmail.com>
2024-12-04 00:42:04 -0500

    Bro Tree and Explosion sword

--- src/main/java/net/pinto/mythandmetal/datagen/ModelItemModelProvider.java
+++ src/main/java/net/pinto/mythandmetal/datagen/ModelItemModelProvider.java
@@ -3,11 +3,13 @@ package net.pinto.mythandmetal.datagen;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
 import net.pinto.mythandmetal.MythandMetal;
+import net.pinto.mythandmetal.block.ModBlocks;
 import net.pinto.mythandmetal.item.ModItems;
 
 public class ModelItemModelProvider extends ItemModelProvider {
@@ -21,7 +23,12 @@ public class ModelItemModelProvider extends ItemModelProvider {
 
 
 
+    }
 
+    private ItemModelBuilder saplingItem(RegistryObject<Block> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(MythandMetal.MOD_ID,"block/" + item.getId().getPath()));
     }
     private ItemModelBuilder handheldItem(RegistryObject<Item> item) {
         return withExistingParent(item.getId().getPath(),
--- src/main/java/net/pinto/mythandmetal/datagen/loot/ModBlockLootDrops.java
+++ src/main/java/net/pinto/mythandmetal/datagen/loot/ModBlockLootDrops.java
@@ -6,27 +6,6 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.registries.RegistryObject;
 import net.pinto.mythandmetal.block.ModBlocks;
-import org.apache.commons.compress.compressors.lz77support.LZ77Compressor;
-
-import java.util.Set;
-
-import net.minecraft.advancements.critereon.StatePropertiesPredicate;
-import net.minecraft.data.loot.BlockLootSubProvider;
-import net.minecraft.world.flag.FeatureFlagSet;
-import net.minecraft.world.flag.FeatureFlags;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.enchantment.Enchantments;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.entries.LootItem;
-import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
-import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
-import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
-import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
-import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
-import net.minecraftforge.registries.RegistryObject;
-
 import java.util.Set;
 
 public class ModBlockLootDrops extends BlockLootSubProvider {
